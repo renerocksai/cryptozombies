4eb78578ee4a7a002a0c549f28beaafad9e9072c
Rene Schallner <rene@rocks.ai>
2021-09-30 02:07:07 +0200

    lesson 2 completed

--- zombiefeeding.sol
+++ zombiefeeding.sol
@@ -2,12 +2,43 @@ pragma solidity >=0.5.0 <0.6.0;
 
 import "./zombiefactory.sol";
 
+interface  KittyInterface {
+    function getKitty(uint256 _id) external view returns (
+        bool isGestating,
+        bool isReady,
+        uint256 cooldownIndex,
+        uint256 nextActionAt,
+        uint256 siringWithId,
+        uint256 birthTime,
+        uint256 matronId,
+        uint256 sireId,
+        uint256 generation,
+        uint256 genes
+    );
+}
+
 contract ZombieFeeding is ZombieFactory {
-    function feedAndMultiply(uint zombieId, uint targetDna) public {
+    // CryptoKittens Address
+    address ckAddress = 0x06012c8cf97BEaD5deAe237070F9587f8E7A266d;
+
+    KittyInterface kittyContract = KittyInterface(ckAddress);
+
+    function feedAndMultiply(uint zombieId, uint targetDna, string memory species) public {
         require(msg.sender == zombieToOwner[zombieId]); 
+
         Zombie storage myZombie = zombies[zombieId];
         uint newDna = (myZombie.dna + (targetDna % dnaModulus)) / 2;
+
+        if(keccak256(species) == keccak256(abi.encodePacked("kitty"))) {
+            newDna = newDna - newDna % 100 + 99;
+        }
         createZombie("NoName", newDna);
+    }
+
+    function feedOnKitty(uint zombieId, uint kittyId) public {
+        uint kittyDna;
+        (,,,,,,,,, kittyDna) = kittyContract.getKitty(kittyId);
+        feedAndMultiply(zombieId, kittyDna, "kitty");
     }
 }
 
